c4efd834b38c5bb4bc9714aeabc9c867c921bf70
ggeugene <g.eugene.g@outlook.com>
2019-03-05 17:23:13 +0200

    Added template file 404.php to display missing pages or pages with wrong url.

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 page
+ * */
+
+get_header(); ?>
+
+    <!-- error-section -->
+<section class="error-section centred">
+    <div class="container">
+        <div class="error-title">404</div>
+        <div class="error-text">Упс, Страница не найдена!</div>
+        <div class="text">Вернуться на <a href="/yasnyy/">Главную.</a></div>
+    </div>
+</section>
+<!-- error-section end -->
+
+<?php get_footer() ?>
