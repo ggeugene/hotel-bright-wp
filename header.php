5a0d01fab720e55b8c5bb6be3caf599d62223030
ggeugene <g.eugene.g@outlook.com>
2019-03-06 12:02:48 +0200

    Added favicon to header.php

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="profile" href="https://gmpg.org/xfn/11" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <link rel="icon" href="/yasnyy/wp-content/themes/yasnyy/images/yasnyy-favicon.png" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 <body class="boxed_wrapper">
