aeb1f69a81f8348e5f0771d9a373ed72b2745e0d
ggeugene <g.eugene.g@outlook.com>
2019-03-06 09:58:01 +0200

    Changed contacts icons at contact.php; Created template file about.php to display About Us page; Fixed margins and padding of short description at about.php.

--- about.php
+++ about.php
@@ -1,26 +1,50 @@
 <?php
 /**
- * Template Name: Contact
+ * Template Name: About
  * */
 
 get_header(); ?>
 
-<section class="page-title sec-pad centred" style="background-image: url(/yasnyy/wp-content/themes/yasnyy/images/background/yasnyy-page-title-min.jpg);">
+<section class="page-title centred" style="background-image: url(/yasnyy/wp-content/themes/yasnyy/images/background/yasnyy-page-title-min.jpg);">
         <div class="container">
             <div class="content-box">
-                <div class="title"><?php the_title() ?></div>
+                <div class="title"><?php the_title(); ?></div>
                 <ul class="bread-crumb">
                     <li><a href="/yasnyy/">Главная</a></li>
-                    <li><?php the_title() ?></li>
+                    <li><?php the_title(); ?></li>
                 </ul>
             </div>
         </div>
-    </section>
-    <!-- page-title end -->
+</section>
 
+<section class="about-section about-us-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                    <div class="single-item-carousel">
+                        <?php $about_images = get_field('about_images');
 
-    <!-- contact-info -->
-    <section class="contact-info">
+                         if( !empty($about_images) ): 
+                            foreach( $about_images as $image ): ?>
+                                <figure class="img-box"><img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>"></figure>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <figure class="img-box"><img src="/yasnyy/wp-content/themes/yasnyy/images/resource/service-2.jpg" alt=""></figure>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 clearfix">
+                    <div class="about-content">
+                        <div class="sec-title left">Отель "Ясный"</div>
+                        <div class="text"><?php echo get_field('desc_field'); ?></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<!-- contact-info -->
+<section class="contact-info">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-12 column">
@@ -43,31 +67,7 @@ get_header(); ?>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- contact-info end -->
+</section>
+<!-- contact-info end -->
 
-
-    <!-- contact-section -->
-    <section class="contact-section gray-bg sec-pad">
-        <div class="container">
-            <div class="title centred"><h2>Свяжитесь с нами</h2></div>
-            <div class="row">
-                <div class="col-lg-8 col-md-12 col-sm-12 offset-lg-2 column">
-                    <div class="default-form">
-                        <?php echo do_shortcode('[contact-form-7 id="70" title="Форма обратной связи"]') ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- contact-section end -->
-
-
-    <!-- google-map -->
-    <section class="google-map-section">
-        <div class="google-map-area">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2649.0762789940354!2d35.066084502258434!3d48.39747826539277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbfb7e89b16861%3A0xc278e14cdc9c5b82!2z0JzQvtGC0LXQu9GMINCv0YHQvdGL0Lk!5e0!3m2!1sru!2sua!4v1551687340502" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-    </section>
-    <!-- google-map end -->
 <?php get_footer(); ?>
